c2ecd342b8d03e0fa80e7c83378beb870fa8c285
MARSIAJ Fabio (Consulting For Edenred) <fabio.marsiaj@consulting-for.edenred.com>
2025-09-11 10:37:35 -0300

    Creating Account domain, service, dto and repository

---
+++ go-api/go.mod
@@ -0,0 +1,5 @@
+module github.com/fabioqmarsiaj/payment-gateway/go-gateway
+
+go 1.25.1
+
+require github.com/google/uuid v1.6.0
---
+++ go-api/internal/domain/errors.go
@@ -0,0 +1,12 @@
+package domain
+
+import "errors"
+
+var (
+	ErrAccountNotFound    = errors.New("account not found")
+	ErrDuplicatedAPIKey   = errors.New("api key already exists")
+	ErrInvoiceNotFound    = errors.New("invoice not found")
+	ErrUnauthorizedAccess = errors.New("unauthorized access")
+	ErrInvalidAmount      = errors.New("invalid amount")
+	ErrInvalidStatus      = errors.New("invalid status")
+)
