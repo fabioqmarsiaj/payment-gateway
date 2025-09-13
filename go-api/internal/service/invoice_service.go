775acf551437c75bdb31e1920977d7fd80d55b05
Fabio Marsiaj <fabiobackend@gmail.com>
2025-09-13 11:42:22 -0300

    Create middleware, invoice service, invoice handler and modify server and main

--- go-api/internal/service/invoice_service.go
+++ go-api/internal/service/invoice_service.go
@@ -7,7 +7,7 @@ import (
 
 type InvoiceService struct {
 	invoiceRepository domain.InvoiceRepository
-	accountservice    AccountService
+	accountService    AccountService
 }
 
 func NewInvoiceService(
@@ -15,17 +15,17 @@ func NewInvoiceService(
 	accountService AccountService) *InvoiceService {
 	return &InvoiceService{
 		invoiceRepository: invoiceRepository,
-		accountservice:    accountService,
+		accountService:    accountService,
 	}
 }
 
-func (s *InvoiceService) Create(input *dto.CreateInvoiceInput) (*dto.InvoiceOutput, error) {
-	accountOutput, err := s.accountservice.FindByAPIKey(input.APIKey)
+func (s *InvoiceService) Create(input dto.CreateInvoiceInput) (*dto.InvoiceOutput, error) {
+	accountOutput, err := s.accountService.FindByAPIKey(input.APIKey)
 	if err != nil {
 		return nil, err
 	}
 
-	invoice, err := dto.ToInvoice(input, accountOutput.ID)
+	invoice, err := dto.ToInvoice(&input, accountOutput.ID)
 	if err != nil {
 		return nil, err
 	}
@@ -35,7 +35,7 @@ func (s *InvoiceService) Create(input *dto.CreateInvoiceInput) (*dto.InvoiceOutp
 	}
 
 	if invoice.Status == domain.StatusApproved {
-		_, err = s.accountservice.UpdateBalance(input.APIKey, invoice.Amount)
+		_, err = s.accountService.UpdateBalance(input.APIKey, invoice.Amount)
 		if err != nil {
 			return nil, err
 		}
@@ -46,4 +46,44 @@ func (s *InvoiceService) Create(input *dto.CreateInvoiceInput) (*dto.InvoiceOutp
 	}
 
 	return dto.FromInvoice(invoice), nil
+}
+
+func (s *InvoiceService) GetByID(id, apiKey string) (*dto.InvoiceOutput, error) {
+	invoice, err := s.invoiceRepository.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	accountOutput, err := s.accountService.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	if invoice.AccountID != accountOutput.ID {
+		return nil, domain.ErrUnauthorizedAccess
+	}
+
+	return dto.FromInvoice(invoice), nil
+}
+
+func (s *InvoiceService) ListByAccount(accountID string) ([]*dto.InvoiceOutput, error) {
+	invoices, err := s.invoiceRepository.FindByAccountID(accountID)
+	if err != nil {
+		return nil, err
+	}
+
+	output := make([]*dto.InvoiceOutput, len(invoices))
+	for i, invoice := range invoices {
+		output[i] = dto.FromInvoice(invoice)
+	}
+	return output, nil
+}
+
+func (s *InvoiceService) ListByAccountAPIKey(apiKey string) ([]*dto.InvoiceOutput, error) {
+	accountOutput, err := s.accountService.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.ListByAccount(accountOutput.ID)
 }
