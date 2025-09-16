ecd1b0672197f32a948a6640539861b2ebbb5f68
Fabio Marsiaj <fabiobackend@gmail.com>
2025-09-16 10:43:35 -0300

    bootstraping prisma db and migrations; docker-compose for db

---
+++ nestjs-anti-fraud/src/invoices/fraud/fraud.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class FraudService {
+
+    constructor(private readonly prismaService: PrismaService){}
+
+    processInvoice() {
+
+    }
+}
